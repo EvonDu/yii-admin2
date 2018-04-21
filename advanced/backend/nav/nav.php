80e0f8781836e8b303ef593084318ca24c991f38
EvonDu <298550274@qq.com>
2018-04-21 11:31:19 +0800

    完成auth的curd功能

--- advanced/backend/nav/nav.php
+++ advanced/backend/nav/nav.php
@@ -16,7 +16,7 @@ class Nav
                     1 => array('title' => "系统用户", 'url' => Url::to(['user/admin/index']), 'auth' => ''),
                     2 => array('title' => "用户管理", 'url' => Url::to(['user/user/index']), 'auth' => ''),
                     3 => array('title' => "角色管理", 'url' => Url::to(['user/role/index']), 'auth' => ''),
-                    4 => array('title' => "权限管理", 'url' => Url::to(['user/auth/permissions']), 'auth' => ''),
+                    4 => array('title' => "权限管理", 'url' => Url::to(['user/auth/index']), 'auth' => ''),
                 )
             ),
         );
